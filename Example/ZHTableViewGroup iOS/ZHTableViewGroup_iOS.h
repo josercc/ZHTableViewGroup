fd2938a6a4fc13f75681d0ac255840126ef98090
josercc <zhanghang@globalegrow.com>
2018-04-17 11:27:46 +0800

    恢复使用默认的头文件

---
+++ Example/ZHTableViewGroup iOS/ZHTableViewGroup_iOS.h
@@ -0,0 +1,21 @@
+//
+//  ZHTableViewGroup_iOS.h
+//  ZHTableViewGroup iOS
+//
+//  Created by 张行 on 2018/4/17.
+//  Copyright © 2018年 15038777234. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZHTableViewGroup_iOS.
+FOUNDATION_EXPORT double ZHTableViewGroup_iOSVersionNumber;
+
+//! Project version string for ZHTableViewGroup_iOS.
+FOUNDATION_EXPORT const unsigned char ZHTableViewGroup_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZHTableViewGroup_iOS/PublicHeader.h>
+
+
+#import "ZHCollectionViewDataSource.h"
+#import "ZHTableViewDataSource.h"
