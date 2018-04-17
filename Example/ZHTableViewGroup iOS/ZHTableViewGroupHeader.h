3780ed4c6db1409689101e18ec9b23dc1df2c345
josercc <zhanghang@globalegrow.com>
2018-04-17 10:04:27 +0800

    新增头文件

---
+++ Example/ZHTableViewGroup iOS/ZHTableViewGroupHeader.h
@@ -0,0 +1,21 @@
+//
+//  ZHTableViewGroupHeader.h
+//  ZHTableViewGroup iOS
+//
+//  Created by 张行 on 2018/4/17.
+//  Copyright © 2018年 15038777234. All rights reserved.
+//
+
+#import "ZHAutoConfigurationCollectionViewDelegate.h"
+#import "ZHAutoConfigurationTableViewDelegate.h"
+#import "ZHCollectionViewBaseModel.h"
+#import "ZHCollectionViewCell.h"
+#import "ZHCollectionViewDataSource.h"
+#import "ZHCollectionViewGroup.h"
+#import "ZHCollectionViewHeaderFooter.h"
+#import "ZHTableViewBaseModel.h"
+#import "ZHTableViewCell.h"
+#import "ZHTableViewDataSource.h"
+#import "ZHTableViewGroup.h"
+#import "ZHTableViewHeaderFooter.h"
+
